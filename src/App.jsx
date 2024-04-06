fd5cdb9b9b01a6ec12d9c543c9855e66866550af
Maxim L <maksim.ldokov.97@mail.ru>
2024-04-06 18:21:46 +0400

    git init

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from './components/Header/Header'
+import Main from './Pages/Main'
+
+import CinemaList from './components/CinemaList/CinemaList'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <CinemaList />
+    </>
+  )
+}
+
+export default App
