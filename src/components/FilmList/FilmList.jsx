bb3dffd986f3b148c21b3232680610cb5cf43644
Maxim L <maksim.ldokov.97@mail.ru>
2024-04-10 16:47:53 +0400

    add the output of films to the page

--- src/components/FilmList/FilmList.jsx
+++ src/components/FilmList/FilmList.jsx
@@ -1,9 +1,30 @@
-import { Pagination } from 'antd'
+import { Pagination, List } from 'antd'
+import FilmItem from '../FilmItem/FilmItem'
 
-const FilmList = () => {
+const FilmList = ({
+  currentPage,
+  totalPages,
+  allFilms,
+  onChange,
+  isLoading,
+}) => {
   return (
     <div>
-      <Pagination defaultCurrent={1} total={500} />
+      <Pagination
+        defaultCurrent={currentPage}
+        total={totalPages}
+        onChange={onChange}
+      />
+      <List
+        loading={isLoading}
+        itemLayout='vertical'
+        size='large'
+        dataSource={allFilms}
+      >
+        {allFilms.map((film) => (
+          <FilmItem film={film} key={film.id} />
+        ))}
+      </List>
     </div>
   )
 }
