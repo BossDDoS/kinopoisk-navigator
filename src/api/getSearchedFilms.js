dee89ac8ef661cb0e4a1260aea856d6314c74479
Maxim L <maksim.ldokov.97@mail.ru>
2024-04-14 14:29:56 +0400

    fix double fetch for search single film by keywords and filters films, add some styles

--- src/api/getSearchedFilms.js
+++ src/api/getSearchedFilms.js
@@ -5,14 +5,26 @@ const BASE_URL = process.env.REACT_APP_BASE_API_URL
 
 export const getSearchedFilms = async (page, limit, query) => {
   try {
-    const response = await axios.get(`${BASE_URL}movie/search`, {
+    const params = {
+      page,
+      limit,
+    }
+
+    if (query) {
+      params.query = query
+    }
+
+    const queryString = Object.keys(params)
+      .map((key) => `${key}=${params[key]}`)
+      .join('&')
+
+    const url = query
+      ? `${BASE_URL}movie/search?${queryString}`
+      : `${BASE_URL}movie?${queryString}`
+
+    const response = await axios.get(url, {
       headers: {
         'X-API-KEY': API_KEY,
-      },
-      params: {
-        page,
-        limit,
-        query,
       },
     })
     return response.data
